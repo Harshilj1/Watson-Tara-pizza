2209f34836168c269b406e9ccfecb15ffef50536
Harshil Janardhan <janardhan.harshil@gmail.com>
2019-08-27 22:17:37 +0530

    Updated

--- app.js
+++ app.js
@@ -20,26 +20,31 @@ mongoose.connect("mongodb+srv://harshil:harshil121@cluster0-cbwck.mongodb.net/te
 })
 
 app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
+    res.send('GET request to the homepage')
 })
 
 app.post('/data', (req, res) => {
-    const post = new Order({
-        order_id : id,
-        name : req.body.name,
-        mobile : req.body.mobile,
-        city : req.body.city,
-        pin : req.body.pin,
-        address : req.body.address,
-        crust_size : req.body.crust_size.split(':')[0],
-        crust_type : req.body.crust_type.split(':')[0],
-        pizza_name : req.body.pizza_name,
-        status : "Your Order is being prepared"
-    })
+    if (req.body.orderId) {
+        console.log("WE have an order")
+    }
+    else {
+        const post = new Order({
+            order_id: id,
+            name: req.body.name,
+            mobile: req.body.mobile,
+            city: req.body.city,
+            pin: req.body.pin,
+            address: req.body.address,
+            crust_size: req.body.crust_size.split(':')[0],
+            crust_type: req.body.crust_type.split(':')[0],
+            pizza_name: req.body.pizza_name,
+            status: "Your Order is being prepared"
+        })
+    }
     console.log(post)
     res.json({
-        success : true,
-        orderId : id
+        success: true,
+        orderId: id
     })
 });
 
