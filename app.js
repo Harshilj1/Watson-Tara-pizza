4754e3bf6f1eb5011a28df181b3458fd0ddd5099
Harshil Janardhan <janardhan.harshil@gmail.com>
2019-08-27 18:17:17 +0530

    update

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 8080
 const app = express()
 const orderMiddleware = require('./routes/post')
+app.use('/',orderMiddleware)
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json())
 app.listen(port, () => {
@@ -13,5 +15,5 @@ app.listen(port, () => {
 mongoose.connect("mongodb+srv://harshil:harshil121@cluster0-cbwck.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true }, () => {
     console.log("Connect to MongoDB")
 })
-app.use('/',orderMiddleware)
+
 
