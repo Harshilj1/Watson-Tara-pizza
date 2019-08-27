26c1a65d70fdaab0976289d82bae2c93ee8ccc67
Harshil Janardhan <janardhan.harshil@gmail.com>
2019-08-27 18:35:59 +0530

    Just Update

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express")
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 8080
+const Order = require('./models/order')
 const app = express()
-const orderMiddleware = require('./routes/post')
-app.use('/',orderMiddleware)
+const time = new Date().getTime().toString()
+const orderid = require('order-id')(time)
+const id = orderid.generate()
+console.log(id)
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json())
@@ -16,4 +19,24 @@ mongoose.connect("mongodb+srv://harshil:harshil121@cluster0-cbwck.mongodb.net/te
     console.log("Connect to MongoDB")
 })
 
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage')
+})
+
+app.post('/data', (req, res) => {
+    const post = new Order({
+        order_id : id,
+        name : req.body.name,
+        mobile : req.body.mobile,
+        city : req.body.city,
+        pin : req.body.pin,
+        address : req.body.address,
+        crust_size : req.body.crust_size.split(':')[0],
+        crust_type : req.body.crust_size.split(':')[0],
+        pizza_name : req.body.pizza_name,
+        status : "Your Order is being prepared"
+    })
+    console.log(post)
+    res.send("Hi")
+});
 
